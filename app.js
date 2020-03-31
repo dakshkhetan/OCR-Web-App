d34dde74e3352cf318194d495498e98416324f6b
Daksh Khetan <2012daksh@gmail.com>
2020-03-31 12:37:43 +0530

    setup view engine and assets path

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const port = 8500;
 const express = require('express');
 const app = express();
 
+// assets
+app.use(express.static('./assets'));
+
+// views
+app.set("view engine", "ejs");
+
 // Server
 app.listen(port, (err) => {
     if(err) return console.log(`Error in setting up server: ${err}`);
