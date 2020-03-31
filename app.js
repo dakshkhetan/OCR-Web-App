a063f31331585a7df164b693129bbacf98118a97
Daksh Khetan <2012daksh@gmail.com>
2020-03-31 12:40:17 +0530

    setup multer

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 const port = 8500;
 const express = require('express');
 const app = express();
+const fs = require('fs');
+const multer = require('multer');
 
 // assets
 app.use(express.static('./assets'));
 
 // views
 app.set("view engine", "ejs");
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "./uploads");
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname);
+    }
+});
+
+// upload function
+const upload = multer({ storage: storage }).single("avatar");
 
 // Server
 app.listen(port, (err) => {
